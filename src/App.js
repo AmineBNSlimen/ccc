3956a70697752d36a595ed1099d51518ce20623f
AmineBNSlimen <100577184+AmineBNSlimen@users.noreply.github.com>
2022-03-08 17:27:49 +0100

    BrowserRouter , Switch and Route are done now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+const HatPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
