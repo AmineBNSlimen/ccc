f92a78e348c13da634652493a1437cdb7607372d
AmineBNSlimen <100577184+AmineBNSlimen@users.noreply.github.com>
2022-03-10 17:16:42 +0100

    redo it yourself man

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.component";
 
-const HatPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+
 function App() {
   return (
     <div>
+      <Header />
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
